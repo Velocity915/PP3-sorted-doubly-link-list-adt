c439dc681dd2b18f48a1fcd93a48e58816ad3749
GSGXander <61768199+GSGXander@users.noreply.github.com>
2025-11-12 22:28:56 -0500

    Added Method Bodies
    Added method bodies for add, remove, size, and isEmpty. Also created the find helper method.

--- adts/DLLSortedList.java
+++ adts/DLLSortedList.java
@@ -11,28 +11,115 @@ public class DLLSortedList<E> implements ListInterface<E>
 
     private int itemAmount = 0;
 
+    private DLLNode<E> find(E element)
+    {
+        DLLNode<E> temp = head;
+	    while (temp != null) 
+        {
+	        if (element.equals(temp.getData())) 
+            {
+	        	return temp;
+	        }
+	        else 
+            {
+                temp = temp.getNext();
+	        }
+	    }
+        return null;
+    }
+    
     @Override
     public void add(E element)
     {
-        
+        DLLNode<E> newNode = new DLLNode<>(element);
+        if(itemAmount == 0)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            DLLNode<E> temp = head;
+            while(temp != null)
+            {
+                if (((Comparable)element).compareTo(temp.getData()) > 0) 
+                {
+                    temp = temp.getNext();
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if(temp == null)
+            {
+                tail.setNext(newNode);
+                newNode.setPrev(tail);
+                tail = newNode;
+            }
+            else if(temp == head)
+            {
+                head.setPrev(newNode);
+                newNode.setNext(head);
+                head = newNode;
+            }
+            else
+            {
+                newNode.setNext(temp);
+                newNode.setPrev(temp.getPrev());
+                temp.getPrev().setNext(newNode);
+                temp.setPrev(newNode);
+            }
+        }
+        itemAmount++;
     }
 
     @Override
     public boolean remove(E element)
     {
-        return false;
+        DLLNode<E> temp = find(element);
+
+        if(temp != null)
+        {
+            if(itemAmount == 1)
+            {
+                head = null;
+                tail = null;
+            }
+            else if(temp == head)
+            {
+                head.getNext().setPrev(null);
+                head = head.getNext();
+            }
+            else if(temp == tail)
+            {
+                tail.getPrev().setNext(null);
+                tail = tail.getPrev();
+            }
+            else
+            {
+                temp.getPrev().setNext(temp.getNext());
+                temp.getNext().setPrev(temp.getPrev());
+            }
+            itemAmount--;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     @Override
     public int size()
     {
-        return 0;
+        return itemAmount;
     }
 
     @Override
     public boolean isEmpty()
     {
-        return false;
+        return itemAmount == 0;
     }
 
     @Override
@@ -59,3 +146,4 @@ public class DLLSortedList<E> implements ListInterface<E>
         return null;
     }
 }
+
