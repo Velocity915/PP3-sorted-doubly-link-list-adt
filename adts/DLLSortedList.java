b25d8671c2e4f086307d508e05cafb52ee51cab3
fergus4eva <158233973+fergus4eva@users.noreply.github.com>
2025-11-19 21:25:00 -0500

    Update DLLSortedList.java
    I have the switch working between the three iterators, i have the logic for the forward iterator and reverse, but I'm still working on the random, i think i might use an array to put all the references in and then a filter array with 000000 each time a number is generated change the 0 to 1 on the filter and return what have is in that index.  
    
    the test for the enhanced for loop looks like this 
    for(Object s : sports) {System.out.print(s + " "); 
    it will call a switch prompt for the user to choose 1, 2 , or 3 for each iterator

--- adts/DLLSortedList.java
+++ adts/DLLSortedList.java
@@ -1,61 +1,270 @@
-package adts;
+package adts;package ADTS;
 
 import nodes.DLLNode;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 import interfaces.ListInterface;
 
-public class DLLSortedList<E> implements ListInterface<E>
+public class DLLSort<E> implements ListInterface<E>, Iterable<E>
 {
     private DLLNode<E> head = null;
     private DLLNode<E> tail = null;
 
     private int itemAmount = 0;
 
+    private DLLNode<E> find(E element)
+    {
+        DLLNode<E> temp = head;
+	    while (temp != null) 
+        {
+	        if (element.equals(temp.getData())) 
+            {
+	        	return temp;
+	        }
+	        else 
+            {
+                temp = temp.getNext();
+	        }
+	    }
+        return null;
+    }
+    
     @Override
     public void add(E element)
     {
-        
+        DLLNode<E> newNode = new DLLNode<>(element);
+        if(itemAmount == 0)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            DLLNode<E> temp = head;
+            while(temp != null)
+            {
+                if (((Comparable)element).compareTo(temp.getData()) > 0) 
+                {
+                    temp = temp.getNext();
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if(temp == null)
+            {
+                tail.setNext(newNode);
+                newNode.setPrev(tail);
+                tail = newNode;
+            }
+            else if(temp == head)
+            {
+                head.setPrev(newNode);
+                newNode.setNext(head);
+                head = newNode;
+            }
+            else
+            {
+                newNode.setNext(temp);
+                newNode.setPrev(temp.getPrev());
+                temp.getPrev().setNext(newNode);
+                temp.setPrev(newNode);
+            }
+        }
+        itemAmount++;
     }
 
     @Override
     public boolean remove(E element)
     {
-        return false;
+        DLLNode<E> temp = find(element);
+
+        if(temp != null)
+        {
+            if(itemAmount == 1)
+            {
+                head = null;
+                tail = null;
+            }
+            else if(temp == head)
+            {
+                head.getNext().setPrev(null);
+                head = head.getNext();
+            }
+            else if(temp == tail)
+            {
+                tail.getPrev().setNext(null);
+                tail = tail.getPrev();
+            }
+            else
+            {
+                temp.getPrev().setNext(temp.getNext());
+                temp.getNext().setPrev(temp.getPrev());
+            }
+            itemAmount--;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     @Override
     public int size()
     {
-        return 0;
+        return itemAmount;
     }
 
     @Override
     public boolean isEmpty()
     {
-        return false;
+        return itemAmount == 0;
     }
+
+	@Override
+	public boolean contains(E element) {
+		DLLNode <E> currentLocation = head; 
+		while(currentLocation.getData() !=null) {
+			if(currentLocation.getData() == element)
+				return true;
+			else 
+				currentLocation = currentLocation.getNext();	
+		}
+			return false;
+	}
+	@Override
+	public E get(E element) {
+		DLLNode <E> currentLocation;
+		currentLocation = head;
+		while(currentLocation != null) {
+			if(currentLocation.getData() == element)
+				return currentLocation.getData();
+			else currentLocation = currentLocation.getNext();
+			
+		}
+		return null;
+	}
+ 
 
     @Override
-    public boolean contains(E element)
-    {
-        return false;
-    }
-
-    @Override
-    public E get(E element)
-    {
-        return null;
-    }
-
-    @Override
-    public E get(int index)
-    {
-        return null;
-    }
-
+public E get(int index) {
+		DLLNode <E> currentLocation; 
+    	if(index == 0) return head.getData();
+		else {
+			currentLocation = head;
+		
+		for(int i = 0; i < index; i++) {
+			currentLocation = currentLocation.getNext();
+			}
+		}
+		return currentLocation.getData();
+	}	
     @Override
     public String toString()
     {
-        return null;
-    }
+    	DLLNode <E> currentLocation = head; 
+		StringBuilder listStr = new StringBuilder("----------------\n");
+    	 for(int i = 0; i < itemAmount; i++) {
+    		listStr.append(currentLocation.getData()+ "\n");
+    		currentLocation = currentLocation.getNext();
+    	}
+		return listStr.toString();
+	}
+
+@Override
+	public Iterator<E> iterator() {
+		int i = getSwitch();
+		
+		if(i == 1)
+		return new forwardIterator();
+		else if(i == 2)
+		return new reverseIterator();
+		else
+		return new randomIterator();
+	}
+	
+	private class forwardIterator implements Iterator<E> {
+		
+		DLLNode<E> current = head;
+		DLLNode<E> temp;
+		@Override
+		public boolean hasNext() {
+			if(current == null)
+				return false;
+			else
+				return true;
+			
+		}
+		@Override
+		public E next() {
+			temp = current;
+			current = current.getNext();
+			return temp.getData();
+			}
 }
+
+	private class reverseIterator implements Iterator<E> {
+		DLLNode<E> current = tail;
+		DLLNode<E> temp;
+		
+		@Override
+		public boolean hasNext() {
+			if(current == null)
+				return false;
+			else
+				return true;
+		}
+
+		@Override
+		public E next() {
+			temp = current;
+			
+			current = current.getPrev();
+			return temp.getData(); 
+		}
+}
+	
+	private class randomIterator implements Iterator<E> {
+		DLLNode<E> current = tail;
+		DLLNode<E> temp;
+		int copysize = size();
+		@Override
+		public boolean hasNext() {
+			if(copysize == 0)
+			return false;
+			else
+				return true;
+		}
+
+		@Override
+		public E next() {
+
+		}
+		
+	}
+
+	private int getSwitch() {
+		Scanner flipSwitch = new Scanner(System.in); // Create a Scanner object
+		System.out.println("Enter which iterator to use:"
+				+ "	1 - forward    2 - reverse     3 - random");
+
+		String input = flipSwitch.nextLine(); // Read user input
+
+
+		int i = Integer.parseInt(input);
+		
+		
+		return i;
+	}
+
+
+
+}
+
+
+
